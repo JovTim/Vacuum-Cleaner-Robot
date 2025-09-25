0386aa2e7b5a7cac8c7fcfa86feac88ff6cfb5b0
JovTim <202280218@psu.palawan.edu.ph>
2025-09-26 00:59:20 +0800

    format changes

--- Program.cs
+++ Program.cs
@@ -3,99 +3,97 @@ using System.ComponentModel.DataAnnotations;
 
 namespace VacuumCleanerRobot
 {
-  public class Program
+  public class Map
   {
+    private enum CellType { Empty, Dirt, Obstacle, Cleaned }
+    private CellType[,] _grid;
+    public int Width { get; private set; }
+    public int Height { get; private set; }
 
-    public class Map
+    public Map(int width, int height)
     {
-      private enum CellType { Empty, Dirt, Obstacle, Cleaned }
-      private CellType[,] _grid;
-      public int Width { get; private set; }
-      public int Height { get; private set; }
+      this.Width = width;
+      this.Height = height;
+      _grid = new CellType[width, height];
 
-      public Map(int width, int height)
+      for (int x = 0; x < width; x++)
       {
-        this.Width = width;
-        this.Height = height;
-        _grid = new CellType[width, height];
-
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-          for (int y = 0; y < height; y++)
-          {
-            _grid[x, y] = CellType.Empty;
-          }
-        }
-      }
-
-      public bool IsInBounds(int x, int y)
-      {
-        return x >= 0 && x < this.Width && y >= 0 && y < this.Height;
-      }
-
-      public bool IsDirt(int x, int y)
-      {
-        return IsInBounds(x, y) && _grid[x, y] == CellType.Dirt;
-      }
-
-      public bool IsObstacles(int x, int y)
-      {
-        return IsInBounds(x, y) && _grid[x, y] == CellType.Obstacle;
-      }
-
-      public void AddObstacle(int x, int y)
-      {
-        _grid[x, y] = CellType.Obstacle;
-      }
-
-      public void AddDirt(int x, int y)
-      {
-        _grid[x, y] = CellType.Dirt;
-      }
-
-      public void Clean(int x, int y)
-      {
-        if (IsInBounds(x, y))
-        {
-          _grid[x, y] = CellType.Cleaned;
-        }
-      }
-
-
-      public void Display(int robotX, int robotY)
-      {
-        // display the 2d grid, it accepts the location of the robot in x and y
-        Console.Clear();
-        Console.WriteLine("Vacuum cleaner robot simulation");
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("Legends: #=Obstacles, D=Dirt, .=Empty, R=Robot, C=Cleaned");
-
-        // display the grid using loop
-        for (int y = 0; y < this.Height; y++)
-        {
-          for (int x = 0; x < this.Width; x++)
-          {
-            if (x == robotX && y == robotY)
-            {
-              Console.Write("R ");
-            }
-            else
-            {
-              switch (_grid[x, y])
-              {
-                case CellType.Empty: Console.Write(". "); break;
-                case CellType.Dirt: Console.Write("D "); break;
-                case CellType.Obstacle: Console.Write("# "); break;
-                case CellType.Cleaned: Console.Write("C "); break;
-
-              }
-            }
-          }
-          Console.WriteLine();
+          _grid[x, y] = CellType.Empty;
         }
       }
     }
 
+    public bool IsInBounds(int x, int y)
+    {
+      return x >= 0 && x < this.Width && y >= 0 && y < this.Height;
+    }
+
+    public bool IsDirt(int x, int y)
+    {
+      return IsInBounds(x, y) && _grid[x, y] == CellType.Dirt;
+    }
+
+    public bool IsObstacles(int x, int y)
+    {
+      return IsInBounds(x, y) && _grid[x, y] == CellType.Obstacle;
+    }
+
+    public void AddObstacle(int x, int y)
+    {
+      _grid[x, y] = CellType.Obstacle;
+    }
+
+    public void AddDirt(int x, int y)
+    {
+      _grid[x, y] = CellType.Dirt;
+    }
+
+    public void Clean(int x, int y)
+    {
+      if (IsInBounds(x, y))
+      {
+        _grid[x, y] = CellType.Cleaned;
+      }
+    }
+
+
+    public void Display(int robotX, int robotY)
+    {
+      // display the 2d grid, it accepts the location of the robot in x and y
+      Console.Clear();
+      Console.WriteLine("Vacuum cleaner robot simulation");
+      Console.WriteLine("---------------------------------");
+      Console.WriteLine("Legends: #=Obstacles, D=Dirt, .=Empty, R=Robot, C=Cleaned");
+
+      // display the grid using loop
+      for (int y = 0; y < this.Height; y++)
+      {
+        for (int x = 0; x < this.Width; x++)
+        {
+          if (x == robotX && y == robotY)
+          {
+            Console.Write("R ");
+          }
+          else
+          {
+            switch (_grid[x, y])
+            {
+              case CellType.Empty: Console.Write(". "); break;
+              case CellType.Dirt: Console.Write("D "); break;
+              case CellType.Obstacle: Console.Write("# "); break;
+              case CellType.Cleaned: Console.Write("C "); break;
+
+            }
+          }
+        }
+        Console.WriteLine();
+      }
+    }
+  }
+  public class Program
+  {
     public static void Main(string[] args)
     {
       Map myMap = new Map(5, 5);
